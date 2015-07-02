efa5a80e37704da0d82992fe8fe53eb9e4dca368
tormozz48 <tormozz48@gmail.com>
2015-07-02 13:27:36 +0300

    #29 Add jsdoc for borschik technology

--- techs/borschik.js
+++ techs/borschik.js
@@ -1,36 +1,3 @@
-/**
- * borschik
- * ========
- *
- * Обрабатывает файл Борщиком (раскрытие borschik-ссылок, минификация, фризинг).
- *
- * Настройки фризинга и путей описываются в конфиге Борщика (`.borschik`) в корне проекта
- * (https://github.com/veged/borschik/blob/master/README.ru.md).
- *
- * **Опции**
- *
- * * *String* **source** — Исходный файл. Например, `?.js`. Обязательная опция.
- * * *String* **target** — Результирующий файл. Например, `_?.js`. Обязательная опция.
- * * *String* **dependantFiles** — Замораживаемые борщиком файлы. Например `['?.css', '?.js']`.
- * * *Boolean* **minify** — Минифицировать ли в процессе обработки. По умолчанию — `true`.
- * * *Boolean* **freeze** — Использовать ли фризинг в процессе обработки. По умолчанию — `false`.
- * * *Boolean* **noCache** — Не использовать кеш для принятия решения о пересборке файла. По умолчанию — `false`.
- * * *String* **tech** — Технология сборки. По умолчанию — соответствует расширению исходного таргета.
- * * *Object* **techOptions** — Параметры для технологии.
- *
- * **Пример**
- *
- * ```javascript
- * nodeConfig.addTech([ require('enb-borschik/techs/borschik'), {
- *   sourceTarget: '?.css',
- *   destTarget: '_?.css',
- *   dependantFiles: ['?.css', '?.js'],
- *   minify: true,
- *   freeze: true,
- *   tech: 'css+'
- * } ]);
- * ```
- */
 var vow = require('vow'),
     preprocessor = new (require('../lib/borschik-preprocessor'))(),
     ProcessorSibling = require('sibling').declare({
@@ -39,6 +6,67 @@ var vow = require('vow'),
         }
     });
 
+/**
+ * @class BorschikTech
+ * @augments {BaseTech}
+ * @classdesc
+ *
+ * Processes file with borschik tool (https://github.com/bem/borschik).
+ *
+ * Can be used for replace and freeze links to images, fonts e.t.c. inside *.css, *.js, *.html source files,
+ * and compress source files.
+ *
+ * Also has ability to override links to *.js and *.css files inside bemtree templates
+ *
+ * @param {Object}      [options]                  Options
+ * @param {String}      [options.target]           Path to target with compiled file.
+ * @param {String}      [options.source]           Path to source file which should be processed.
+ * @param {String[]}    [options.dependantFiles]   Files with specified suffixes must be built before this tech execution.
+ * @param {Boolean}     [options.minify=true]      Minimize file during borschik processing.
+ * @param {Boolean}     [options.freeze=false]     Freeze links to sources.
+ * @param {Boolean}     [options.noCache=false]    Forcibly drop cache usage.
+ * @param {String}      [options.tech]             Technology for borschik processing. By default it corresponds to source file extension.
+ * @param {Object}      [options.techOptions]      Params for 'tech' technology option
+ *
+ * @example
+ * var CssStylus = require('enb-stylus/techs/stylus'),
+ *     CssAutoprefixer = require('enb-autoprefixer/techs/css-autoprefixer'),
+ *     Borschik = require('enb-borschik/techs/borschik'),
+ *     FileProvideTech = require('enb/techs/file-provider'),
+ *     bem = require('enb-bem-techs');
+ *
+ * module.exports = function(config) {
+ *     config.node('bundle', function(node) {
+ *         // get FileList
+ *         node.addTechs([
+ *             [FileProvideTech, { target: '?.bemdecl.js' }],
+ *             [bem.levels, levels: ['blocks']],
+ *             bem.deps,
+ *             bem.files
+ *         ]);
+ *
+ *         // build css file
+ *         node.addTechs([
+ *            [CssStylus, { target: '?.noprefix.css' }],
+ *            [CssAutoprefixer, {
+ *                sourceTarget: '?.noprefix.css',
+ *                destTarget: '?.css',
+ *                browserSupport: ['last 2 versions', 'ie 10', 'opera 12.16']
+ *            }]
+ *         ]);
+ *
+ *         // minimize and freeze links inside *.css file by borschik
+ *         node.addTech([Borschik, {
+ *             source: '?.css',
+ *             target: '_?.css',
+ *             tech: 'cleancss',
+ *             freeze: true,
+ *             minify: true
+ *         }]);
+ *         node.addTarget('_?.css');
+ *     });
+ * };
+ */
 module.exports = require('enb/lib/build-flow').create()
     .name('borschik')
     .target('target')
